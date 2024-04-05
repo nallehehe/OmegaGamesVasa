e1261b5f6fe5336ae9562f017fd782d538af4e16
MrFrolin <Philip_frolin@hotmail.com>
2024-04-05 10:31:52 +0200

    Added endpointExtension #153 Product entity added #172

---
+++ OmegaGamesVasa/OmegaGamesAPI/Extensions/ProductEndpointExtension.cs
@@ -0,0 +1,26 @@
+﻿namespace OmegaGamesAPI.Extensions;
+
+public static class ProductEndpointExtension
+{
+    public static IEndpointRouteBuilder MapProductsEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("products");
+
+        group.MapGet("/", GetAllProducts);
+
+        group.MapGet("/{id}", GetProductById);
+
+        return app;
+    }
+
+    private static Task<IResult> GetAllProducts(HttpContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Task GetProductById(HttpContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
+
