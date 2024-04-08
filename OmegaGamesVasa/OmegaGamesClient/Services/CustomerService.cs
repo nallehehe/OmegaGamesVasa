295b9a4c723595b2593679801fd7f40c59598642
SebMoya <sebastianmoya@live.se>
2024-04-08 09:51:55 +0200

    update customer service with customer object #180

---
+++ OmegaGamesVasa/OmegaGamesClient/Services/CustomerService.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Common.DTO;
+
+namespace OmegaGamesAPI.Services;
+
+public class CustomerService
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    [EmailAddress]
+    public string Email { get; set; }
+    [Phone]
+    public string Phone { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string ZipCode { get; set; }
+    public string Country { get; set; }
+    public List<ProductDTO> Cart { get; set; }
+
+    public CustomerService()
+    {
+        Cart = new List<ProductDTO>();
+    }
+
+    public void AddToCart(ProductDTO product)
+    {
+        Cart.Add(product);
+    }
+
+    public void RemoveFromCart(ProductDTO product)
+    {
+        Cart.Remove(product);
+    }
+
+    public void ClearCart()
+    {
+        Cart.Clear();
+    }
+
+}
