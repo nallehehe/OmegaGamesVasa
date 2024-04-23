f8343fc5dcfec5eea509c84865b8f1c1bdf98662
Uberoy <joacim.jansson@iths.se>
2024-04-23 13:56:12 +0200

    Created DTO for KlarnaOrder and related objects

---
+++ OmegaGamesVasa/Common/DTO/Klarna/KlarnaDeliveryDetailsDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.DTO.Klarna;
+
+public class KlarnaDeliveryDetailsDTO
+{
+    public string carrier { get; set; }
+    public KlarnaProduct product { get; set; }
+    public KlarnaTimeSlotDTO timeslot { get; set; }
+    public KlarnaPickupLocationDTO pickup_location { get; set; }
+}
---
+++ OmegaGamesVasa/Common/DTO/Klarna/KlarnaShippingAttributesDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Common.DTO.Klarna;
+
+public class KlarnaShippingAttributesDTO
+{
+    public int weight { get; set; }
+    public Dimensions dimensions { get; set; }
+    public List<string> tags { get; set; }
+}
